9206c7f7417654aee5c4c775c8d367da57c0b252
ckle04 <mailcooper@gmail.com>
2025-02-25 14:05:08 -0500

    Update ui.ts
    Added more personalization to the UI

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Stockbot`;
+export const MESSAGE_PLACEHOLDER: string = `Thinking about how to increase the wealth gap...`;
 export const FOOTER_MESSAGE: string = `Customized AI by Cooper Klein`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
