bab4203aa03c9192568a4532ce3c9383c6307da3
ckle04 <mailcooper@gmail.com>
2025-03-16 11:28:42 -0400

    Update identity.ts Corrected Identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Cooper Klein`;
 export const OWNER_DESCRIPTION: string = `UNC Chapel Hill Business Student`;
 
-export const AI_NAME: string = `Coopers`;
+export const AI_NAME: string = `FriendlyFi`;
 export const AI_TONE: string = `Friendly`;
 export const AI_ROLE: string = `Assistant`;
