16af460b3aea8d8d86c78d851205ce36115a2494
ckle04 <mailcooper@gmail.com>
2025-03-16 11:17:33 -0400

    Update ui.ts FriendlyFi page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `FriendlyFi Data`;
+export const CHAT_HEADER: string = `FriendlyFi`;
 export const MESSAGE_PLACEHOLDER: string = `Thinking about how to increase the wealth gap...`;
 export const FOOTER_MESSAGE: string = `Customized AI by Cooper Klein`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `FriendlyFi`;
+export const PAGE_TITLE: string = `FriendlyFi Buddy`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
