3fd265d180fe4868151a1afdd12111dd21ec5445
ckle04 <mailcooper@gmail.com>
2025-03-16 13:58:57 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `Thinking about how to increase the w
 export const FOOTER_MESSAGE: string = `Customized AI by Cooper Klein`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `FriendlyFi Buddy`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s Financial AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
