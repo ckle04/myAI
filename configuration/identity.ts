9b9bf37bcdaefd5f786d9cea2de6edcf6ac8a6eb
ckle04 <mailcooper@gmail.com>
2025-03-16 11:13:06 -0400

    Update identity.ts with Personal Information

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Cooper Klein`;
+export const OWNER_DESCRIPTION: string = `UNC Chapel Hill Business Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Coopers`;
+export const AI_TONE: string = `Friendly`;
+export const AI_ROLE: string = `Assistant`;
